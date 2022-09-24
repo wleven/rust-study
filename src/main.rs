0e8c6484d5125c53e0b503ac015c22b87b5f74bc
wleven <408493323@qq.com>
2022-09-24 18:36:14 +0800

    feat: calc_area

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,34 @@
 // pub mod guess_game;
 
+mod calc_area;
 mod guess_game;
 
 fn main() {
-    guess_game::start()
+    let list = ["1. guess_game", "2. calc_area"];
+
+    for item in list.iter() {
+        println!("{}", item)
+    }
+
+    let mut test_id = String::new();
+
+    println!("Please input test id");
+    std::io::stdin().read_line(&mut test_id).expect("Err");
+
+    let test_id: &str = test_id.trim();
+
+    match test_id {
+        "1" => guess_game::start(),
+
+        "2" => {
+            let area = calc_area::calc(&calc_area::React {
+                width: 10,
+                height: 2,
+            });
+
+            println!("The react area is {}", area)
+        }
+
+        _ => println!("Test id is invalid"),
+    }
 }
